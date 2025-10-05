f9c0cc1b606fbb224be5387f0a9998d9286d365a
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-10-06 00:58:51 +0100

    Update Header.tsx

--- alx-project-0x05/components/layout/Header.tsx
+++ alx-project-0x05/components/layout/Header.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { RootState } from "@/store/store";
-import { useSelector } from "react-redux";
+import { useCount } from "@/context/CountContext";
 
 const Header: React.FC = () => {
   const pathname = usePathname();
-  const count = useSelector((state: RootState) => state.counter.value);
+  const { count } = useCount();
 
   return (
     <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">Splash App</Link>
+        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+          Splash App
+        </Link>
+
         <div className="flex gap-4">
           {!["/counter-app"].includes(pathname) ? (
             <>
@@ -19,7 +21,7 @@ const Header: React.FC = () => {
               <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
             </>
           ) : (
-            <p className="font-semibold text-lg">Current count : {count}</p>
+            <p className="font-semibold text-lg">Current count: {count}</p>
           )}
         </div>
       </div>
